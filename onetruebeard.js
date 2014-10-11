9790132a28a81f85a052f6e3b42b72cd2de97a0d
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2014-10-11 14:34:05 -0400

    shorter because it's funner

--- onetruebeard.js
+++ onetruebeard.js
@@ -1,22 +1,19 @@
 $(document).ready(function() {
   
-    var earlImg = ['http://i.imgur.com/QIvruce.jpg',
-                   'http://i.imgur.com/aSsSEw9.jpg',
-                   'http://i.imgur.com/As9OXDT.jpg',
-                   'http://i.imgur.com/wcfN9Lb.jpg',
-                   'http://i.imgur.com/xTvUEXQ.jpg',
-                   'http://i.imgur.com/tUMPCw2.jpg',
-                   'http://i.imgur.com/kva4BWc.jpg',
-                   'http://i.imgur.com/eQn0vQO.jpg',
-                   'http://i.imgur.com/mrE5CCK.png',
-                   'http://i.imgur.com/XaPr6jp.jpg',
+    var earlImg = ['QIvruce.jpg',
+                   'aSsSEw9.jpg',
+                   'As9OXDT.jpg',
+                   'wcfN9Lb.jpg',
+                   'xTvUEXQ.jpg',
+                   'tUMPCw2.jpg',
+                   'kva4BWc.jpg',
+                   'eQn0vQO.jpg',
+                   'mrE5CCK.png',
+                   'XaPr6jp.jpg',
                   ];
     
-    var oldImg = $('img');
-    
-    oldImg.each(function() {
-        var a = $(this);
-        a.attr("src", earlImg[Math.floor(Math.random()*earlImg.length)]);
+    $("img").each(function() {
+        $(this).attr("src", "http://i.imgur.com/" + earlImg[Math.floor(Math.random()*earlImg.length)]);
     });
 
 });
